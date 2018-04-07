0a76a96e48a0309f4794785d3821ac8f9d661489
johnnunley <jtnunley01@gmail.com>
2018-04-07 08:00:48 -0700

    Added diagnostics

--- webrpg.js
+++ webrpg.js
@@ -261,6 +261,7 @@ webrpg.Frame.prototype.render = function() {
   var room = webrpg._internalFunctions.roomArray(this.room.width,this.room.height);
   for (var i = 0; i < this.room.entities.length; i++) {
     var entity = this.room.entities[i];
+    console.log(entity);
     room[entity.x][entity.y] = '<span style="color: ' + entity.color+'">' + webrpg.frameProperties.rmCharacter + '</span>';
   }
   for (var i = 0; i < room.length; i++) {
